e764f561357af18935c545372fcabc6c984b4972
Martin Georges <mgeorges@c3r8s6.42luxembourg.lu>
2025-05-13 10:13:16 +0200

    few changes in ex00

--- ex00/Cat.hpp
+++ ex00/Cat.hpp
@@ -6,7 +6,25 @@
 /*   By: mgeorges <mgeorges@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 08:24:39 by mgeorges          #+#    #+#             */
-/*   Updated: 2025/05/13 08:24:40 by mgeorges         ###   ########.fr       */
+/*   Updated: 2025/05/13 10:12:28 by mgeorges         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CAT_HPP
+#define CAT_HPP
+
+#include <iostream>
+#include "Animal.hpp"
+
+class Cat : public Animal
+{
+    public:
+        Cat();
+        Cat(const Cat &copy);
+        ~Cat();
+        Cat & operator = (const Cat &other);
+
+
+};
+
+#endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,17 +6,19 @@
 /*   By: mgeorges <mgeorges@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 10:19:55 by mgeorges          #+#    #+#             */
-/*   Updated: 2025/05/12 12:02:36 by mgeorges         ###   ########.fr       */
+/*   Updated: 2025/05/13 10:11:20 by mgeorges         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
 
 int main()
 {
     const Animal* meta = new Animal();
     const Animal* j = new Dog();
-    const Animal* i = new cat();
+    const Animal* i = new Cat();
 
     std::cout << j->getType() << " " << std::endl;
     std::cout << i->getType() << " " << std::endl;
